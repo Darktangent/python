085897ea09d7132d704d9057041e046b8ee65e90
Rohan Ganguly <rohan.ganguly@snhu.edu>
2018-05-07 13:52:00 +0000

    hello script

---
+++ hello.py
@@ -0,0 +1,11 @@
+#This program says hello and for a name
+print ('Hello World!')
+print ('What  is your name?') #ask for name
+Name = input()
+print ('it is good to see you, ' + Name)
+print ('The length of your name is')
+print (len(Name))
+print ('What is your age') #ask for age
+age =input()
+print ('You  will be' + str(int(age)+2) + 'in two years')
+
